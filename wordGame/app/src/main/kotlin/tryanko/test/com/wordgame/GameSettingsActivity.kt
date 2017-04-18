bd04b1c526a642c4982b6256edacd09719f65237
SLEEPwalker131313 <petrunin-vn@yandex.ru>
2017-04-18 22:55:34 +0300

    Game Settings
    Заготовка активити для создания новой игры

--- wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/GameSettingsActivity.kt
+++ wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/GameSettingsActivity.kt
@@ -1,12 +1,12 @@
 package tryanko.test.com.wordgame
 
+import android.R
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
-import org.jetbrains.anko.editText
-import org.jetbrains.anko.linearLayout
-import org.jetbrains.anko.onClick
-import org.jetbrains.anko.verticalLayout
+import android.widget.ArrayAdapter
+import org.jetbrains.anko.*
 
 class GameSettingsActivity : AppCompatActivity() {
 
@@ -16,11 +16,39 @@ class GameSettingsActivity : AppCompatActivity() {
             linearLayout {
                 editText("Player 1") {
                     setSelectAllOnFocus(true)
+                }.lparams{
+                    weight = 1F
                 }
-                editText("Player 2") {
+                editText("Player 22") {
                     setSelectAllOnFocus(true)
+                }.lparams{
+                    weight = 1F
                 }
             }
+            textView("Размер поля: ")
+                    .lparams {
+                        gravity = left
+                        width = wrapContent
+                    }
+            val tmp = ArrayAdapter<String>(applicationContext, R.layout.simple_spinner_item, arrayOf("4x4", "5x5", "6x6", "7x7"))
+            spinner {
+                adapter = tmp
+//                setSelection(2)
+            }
+            textView("Слово: ")
+                    .lparams {
+                        gravity = left
+                        width = wrapContent
+                    }
+            editText("Случайное")
+                    .lparams {
+                        gravity = left
+                        width = wrapContent
+                    }
+            button("Начать игру")
+                    .lparams {
+                        width = matchParent
+                    }
         }
     }
 }
