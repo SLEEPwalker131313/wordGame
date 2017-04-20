f0d6460f93414b895ed5767877df6b254c5c9a33
SLEEPwalker131313 <petrunin-vn@yandex.ru>
2017-04-20 10:14:18 +0300

    Refactoring+стили для подсветки текущего игрока

--- wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/GameActivity.kt
+++ wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/GameActivity.kt
@@ -2,10 +2,12 @@ package tryanko.test.com.wordgame
 
 import android.content.Intent
 import android.graphics.Color
+import android.media.JetPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
+import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
@@ -23,14 +25,25 @@ class GameActivity : AppCompatActivity() {
         val time = intent.getStringExtra("time")
         val word = intent.getStringExtra("word")
         var startText = ""
+        var isPlayer1Turn = true
+        var gameIsOver = false
         val fieldMatrix = Array(fieldSize+1, {ArrayList<EditText>()})
         //TODO: Попробовать вернуть пример с координатами в лог при таче корневого вью
         //TODO: (возможно получится отловить коотдинаты и так добиться плавных переходов между разными EditText)
         //TODO: Доделать базу. Добавить очки и использование в игре слова
         verticalLayout {
             linearLayout {
-                textView(player1name)
-                textView(player2name)
+                textView(player1name){
+                    id = ViewIDEnum.PLAYER_1_NAME_TEXT_VIEW_ID.id
+                    gravity = Gravity.CENTER
+                }.lparams { weight = 1F }
+                textView("--|--"){
+                    gravity = Gravity.CENTER
+                }.lparams { weight = 1F }
+                textView(player2name){
+                    id = ViewIDEnum.PLAYER_2_NAME_TEXT_VIEW_ID.id
+                    gravity = Gravity.CENTER
+                }.lparams { weight = 1F }
             }
             textView(fieldSize.toString())
             textView(time)
@@ -83,11 +96,16 @@ class GameActivity : AppCompatActivity() {
                 }
             }
             linearLayout {
-                button("Отмена")
+                button("Отмена"){
+                    id = ViewIDEnum.BTN_ROLLBACK_BUTTON_ID.id
+                }
                 textView {
                     id = ViewIDEnum.CURRENT_WORD_TEXT_VIEW_ID.id
                 }
-                button("OK")
+                button("OK"){
+                    id = ViewIDEnum.BTN_OK_BUTTON_ID.id
+                }
+
                 editText("ds"){
                     setOnTouchListener { v, event ->
                         if(event.action == MotionEvent.ACTION_DOWN)
@@ -95,11 +113,23 @@ class GameActivity : AppCompatActivity() {
                         true
                     }
                 }
+
             }
 
             var currentWord = find<TextView>(ViewIDEnum.CURRENT_WORD_TEXT_VIEW_ID.id)
             fieldMatrix[0][0].onClick { currentWord.text = currentWord.text.toString() + fieldMatrix[0][0].text.toString() }
         }
+
+        find<Button>(ViewIDEnum.BTN_OK_BUTTON_ID.id).onClick {
+            isPlayer1Turn = !isPlayer1Turn
+            selectCurrentPlayer(isPlayer1Turn)
+
+        }
+
+//        while(gameIsOver){
+//            selectCurrentPlayer(isPlayer1Turn)
+//            gameIsOver = false
+//        }
 
 //        val fieldView = find<GridLayout>(ViewIDEnum.GAME_FIELD_GRID_LAYOUT_ID.id)
 //        fieldView
@@ -111,6 +141,27 @@ class GameActivity : AppCompatActivity() {
 //            }
 //            true
 //        }
+    }
+
+    //Применить стили в зависимости от того чей ход
+    fun selectCurrentPlayer(isPlayer1Turn: Boolean){
+        val player1nameTextView = find<TextView>(ViewIDEnum.PLAYER_1_NAME_TEXT_VIEW_ID.id)
+        val player2nameTextView = find<TextView>(ViewIDEnum.PLAYER_2_NAME_TEXT_VIEW_ID.id)
+        if(isPlayer1Turn){
+            activePlayerTextViewStyle(player1nameTextView)
+            nonActivePlayerTextViewStyle(player2nameTextView)
+        } else{
+            activePlayerTextViewStyle(player2nameTextView)
+            nonActivePlayerTextViewStyle(player1nameTextView)
+        }
+    }
+
+    private fun nonActivePlayerTextViewStyle(playerNameTextView: TextView) {
+        playerNameTextView.backgroundColor = Color.WHITE
+    }
+
+    private fun activePlayerTextViewStyle(playerNameTextView: TextView) {
+        playerNameTextView.backgroundColor = Color.RED
     }
 
     //Стиль для основного игрвоого поля
@@ -135,7 +186,8 @@ class GameActivity : AppCompatActivity() {
         backgroundColor = Color.GRAY
     }
 
-    private fun isNearlyMiddleRow(fieldSize: Int, i: Int) = i == fieldSize / 2 - 1 || i == fieldSize / 2 + 1
+    private fun isNearlyMiddleRow(fieldSize: Int, i: Int) =
+            i == fieldSize / 2 - 1 || i == fieldSize / 2 + 1
 
     private fun isMiddleRow(fieldSize: Int, i: Int) = i == fieldSize / 2
 
