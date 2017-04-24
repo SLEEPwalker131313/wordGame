e1a83be07f29119708f9173cced8d74be597dfd7
SLEEPwalker131313 <petrunin-vn@yandex.ru>
2017-04-24 11:18:32 +0300

    View
    Добавлен пример последовательного выделения свайпом. Либо для linear, но
    так выделение работает только строкам, либо gridLayout но там нету
    weight

--- wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/StartActivity.kt
+++ wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/StartActivity.kt
@@ -31,71 +31,97 @@ class StartActivity : AppCompatActivity() {
                 text = "Правила"
             }
             //ВЫделение последовательных текствью
-            linearLayout {
-                onTouch { v, event ->
-                    if(event.action == MotionEvent.ACTION_DOWN){
+            verticalLayout {
+//                requestChildFocus(it, this)
+//                onTouch { v, event ->
+//                    if(event.action == MotionEvent.ACTION_DOWN){
 //                        toast("Hi!")
-//                        backgroundColor = Color.RED
-
-                    }
-                    if(event.action == MotionEvent.ACTION_MOVE) {
-
-                        forEachChild {
-//                            Log.d("forEach", it.toString())
-                            Log.d("myCoord", "x: ${event.x} y: ${event.y}")
-                            Log.d("forEach", "left: ${it.left} right: ${it.right} top: ${it.top} bot: ${bottom}")
-                            event.viewIsTouched(it)
-                        }
-//                        Log.d("focusedChild", focusedChild.)
-//                        backgroundColor = Color.GREEN
-//                        event.viewIsTouched(child)
-//                        event.viewIsTouched(find<TextView>(102))
-//                        event.viewIsTouched(find<TextView>(103))
-//                        event.viewIsTouched(find<TextView>(104))
-//                        focusedChild.backgroundColor = Color.GREEN
-//                        v.childrenRecursiveSequence()
-//                        v.backgroundColor = Color.GREEN
-//                        backgroundColor = Color.GREEN
-//                        focusedChild.backgroundColor = Color.GREEN
-                    }
-                    true
-                }
-                for(i in 0..2){
-                    //Работает с textView
-                    editText(i.toString()){
-                        cursorVisible = false
-                        showSoftInputOnFocus = false
-                        touchscreenBlocksFocus = true
-//                        onClick{
-//                            backgroundColor = Color.RED
+////                        backgroundColor = Color.RED
+//
+//                    }
+//                    if(event.action == MotionEvent.ACTION_MOVE) {
+//                        forEachChild {
+////                            Log.d("forEach", it.toString())
+//                            Log.d("myCoord", "x: ${event.x} y: ${event.y}")
+//                            Log.d("forEach", "left: ${it.left} right: ${it.right} top: ${it.top} bot: ${bottom}")
+//                            event.viewIsTouched(it)
 //                        }
-                        onTouch { view, motionEvent ->
-                            if(motionEvent.action == MotionEvent.ACTION_POINTER_DOWN){
-                                toast("HI! ACTION_POINTER_DOWN")
+////                        Log.d("focusedChild", focusedChild.)
+////                        backgroundColor = Color.GREEN
+////                        event.viewIsTouched(child)
+////                        event.viewIsTouched(find<TextView>(102))
+////                        event.viewIsTouched(find<TextView>(103))
+////                        event.viewIsTouched(find<TextView>(104))
+////                        focusedChild.backgroundColor = Color.GREEN
+////                        v.childrenRecursiveSequence()
+////                        v.backgroundColor = Color.GREEN
+////                        backgroundColor = Color.GREEN
+////                        focusedChild.backgroundColor = Color.GREEN
+//                    }
+//                    true
+//                }
+                for(j in 0 ..5)
+                    linearLayout {
+                        for (i in 0..5) {
+                            editText(i.toString()) {
+                                onTouch { view, motionEvent ->
+                                    if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                                        toast("HI!ACTION_DOWN")
+                                        Log.d("parent", parent.toString())
+                                    }
+                                    if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+                                        forEachChild {
+                                            Log.d("myCoord", "x: ${motionEvent.x + i*(it.right - it.left)} y: ${motionEvent.y}")
+                                            Log.d("forEach", "i: $i j: $j left: ${it.left} right: ${it.right} top: ${it.top} bot: ${bottom}")
+                                            Log.d("parent", parent.toString())
+
+                                            if(motionEvent.x + i*(it.right - it.left) >= it.left && motionEvent.x + i*(it.right - it.left) <= it.right) {
+                                                if(motionEvent.y >= it.top && motionEvent.y <= it.bottom) {
+                                                    Log.d("Target", "yeah!")
+                                                    it.setBackgroundColor(Color.GREEN)
+                                                }
+                                            }
+
+                                        }
+                                    }
+                                    false
+                                }
                             }
-                            if(motionEvent.action == MotionEvent.ACTION_BUTTON_PRESS){
-                                toast("HI! ACTION_BUTTON_PRESS")
-                            }
-                            if(motionEvent.action == MotionEvent.ACTION_DOWN){
-                                toast("HI!ACTION_DOWN")
-                                Log.d("parent", parent.toString())
-                            }
-                            if(motionEvent.action == MotionEvent.ACTION_MOVE){
-                                toast("HI! ACTION_MOVE")
-                                Log.d("parent", parent.toString())
-                                backgroundColor = Color.RED
-//                                onFocusChange { view, b -> motionEvent.viewIsTouched(view)
-//                                    Log.d("check","view") }
-                            }
-                            if(motionEvent.action == MotionEvent.ACTION_MASK){
-                                toast("HI! ACTION_MASK")
-                            }
-                            if(motionEvent.action == MotionEvent.ACTION_HOVER_MOVE)
-                                backgroundColor = Color.RED
-                            false
                         }
                     }
-                }
+            }.lparams(width = matchParent)
+
+            gridLayout {
+                rowCount = 6
+                columnCount = 6
+                for(j in 0 ..5)
+                        for (i in 0..5) {
+                            editText(i.toString()) {
+                                onTouch { view, motionEvent ->
+                                    if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                                        toast("HI!ACTION_DOWN")
+                                        Log.d("parent", parent.toString())
+                                    }
+                                    if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+                                        forEachChild {
+                                            Log.d("myCoord", "x: ${motionEvent.x + i*(it.right - it.left)} y: ${motionEvent.y}")
+                                            Log.d("forEach", "i: $i j: $j left: ${it.left} right: ${it.right} top: ${it.top} bot: ${bottom}")
+                                            Log.d("parent", parent.toString())
+
+                                            if(motionEvent.x + i*(it.right - it.left) >= it.left && motionEvent.x + i*(it.right - it.left) <= it.right) {
+                                                if(motionEvent.y >= it.top && motionEvent.y <= it.bottom) {
+                                                    Log.d("Target", "yeah!")
+                                                    it.setBackgroundColor(Color.GREEN)
+                                                }
+                                            }
+
+                                        }
+                                    }
+                                    false
+                                }
+                            }
+                        }
+
             }
         }
         database.use {
@@ -124,7 +150,7 @@ class StartActivity : AppCompatActivity() {
 
 fun MotionEvent.viewIsTouched(view: View){
     if(x >= view.left && x <= view.right) {
-        if(y >= view.top && x <= view.bottom) {
+        if(y >= view.top && y <= view.bottom) {
             Log.d("Target", "yeah!")
             view.setBackgroundColor(Color.GREEN)
         }
