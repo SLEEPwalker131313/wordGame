3f15ae7be7c7c1d458b33071144382e1c3040183
SLEEPwalker131313 <petrunin-vn@yandex.ru>
2017-04-25 10:24:39 +0300

    IU
    Доработан внешний вид первого активити. Добавлен alertDialog для
    отображения правил игры

--- wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/StartActivity.kt
+++ wordGame/app/src/main/kotlin/tryanko/test/com/wordgame/StartActivity.kt
@@ -4,15 +4,10 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
-import android.view.MotionEvent
-import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import org.jetbrains.anko.*
-import org.jetbrains.anko.db.parseList
-import org.jetbrains.anko.db.rowParser
-import org.jetbrains.anko.db.select
-import tryanko.test.com.wordgame.GameActivity
 
 class StartActivity : AppCompatActivity() {
 
@@ -20,41 +15,87 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         verticalLayout {
             background = resources.getDrawable(R.mipmap.main_background)
-            textView{
+            textView {
                 text = StringConstantEnum.START_GAME_STRING_CONSTANT.text
-                typeface = Typeface.createFromAsset(assets, "fonts/BadScript-Regular.ttf")
+                applyCustomFont()
                 textSize = 40F
                 textColor = Color.BLACK
                 gravity = Gravity.CENTER_HORIZONTAL
                 gravity = Gravity.BOTTOM
-                onClick{
+                onClick {
                     startActivity<GameSettingsActivity>()
                 }
-            }.lparams(weight = 1F){
+            }.lparams(weight = 1F) {
                 gravity = Gravity.CENTER
             }
-            textView{
+            textView {
                 text = StringConstantEnum.RULES_WORD_CONSTANT.text
-                typeface = Typeface.createFromAsset(assets, "fonts/BadScript-Regular.ttf")
+                applyCustomFont()
                 textSize = 40F
                 textColor = Color.BLACK
                 gravity = Gravity.CENTER_HORIZONTAL
                 gravity = Gravity.TOP
-            }.lparams(weight = 1.5F){
+                onClick {
+                    alert {
+                        customTitle {
+                            linearLayout {
+                                imageView(resources.getDrawable(R.mipmap.rules_title_icon))
+                                        .lparams {
+                                            leftMargin = 30
+                                            topMargin = 20
+                                        }
+                                textView {
+                                    text = StringConstantEnum.RULES_WORD_CONSTANT.text
+                                    applyCustomFont()
+                                    textSize = 30F
+                                    gravity = Gravity.CENTER
+                                }.lparams {
+                                    leftMargin = 20
+                                    topMargin = 20
+                                }
+                            }
+                        }
+                        customView {
+                            scrollView {
+                                background = resources.getDrawable(R.mipmap.paper)
+                                verticalLayout {
+                                    textView {
+                                        text = StringConstantEnum.MAIN_RULES_TEXT_STRING_CONSTANT.text
+                                        gravity = Gravity.CENTER_HORIZONTAL
+                                        textColor = Color.BLACK
+                                    }.lparams { marginLeftRight() }
+                                    textView {
+                                        text = StringConstantEnum.RULES_TITLE_TEXT_STRING_CONSTANT.text
+                                        textColor = Color.BLACK
+                                        textSize = 17F
+                                    }.lparams { marginLeftRight() }
+                                    textView {
+                                        text = StringConstantEnum.RULE_1_STRING_CONSTANT.text
+                                    }.lparams { marginLeftRight() }
+                                    textView {
+                                        text = StringConstantEnum.RULE_2_STRING_CONSTANT.text
+                                    }.lparams { marginLeftRight() }
+                                    textView {
+                                        text = StringConstantEnum.RULE_3_STRING_CONSTANT.text
+                                    }.lparams { marginLeftRight() }
+                                    textView {
+                                        text = StringConstantEnum.RULE_4_STRING_CONSTANT.text
+                                    }.lparams { marginLeftRight() }
+                                    textView {
+                                        text = StringConstantEnum.RULE_5_STRING_CONSTANT.text
+                                    }.lparams { marginLeftRight() }
+                                }
+                            }
+                        }
+                        positiveButton(StringConstantEnum.CLOSE_STRING_CONSTANT.text) {}
+                    }.show()
+                }
+            }.lparams(weight = 1.5F) {
                 gravity = Gravity.CENTER
             }
-
-
-
-//            button {
-//                text = StringConstantEnum.START_GAME_STRING_CONSTANT.text
-//                onClick{
-//                    startActivity<GameSettingsActivity>()
-//                }
-//            }
         }
         database.use {
-//            database.createUsedWordsTable(database.readableDatabase)
+            //            database.createUsedWordsTable(database.readableDatabase)
 //            database.createGameTable(database.readableDatabase)
 
 //            database.dropGameTable(database.readableDatabase)
@@ -74,14 +115,19 @@ class StartActivity : AppCompatActivity() {
 //            database.dropUsedWordsTable(database.readableDatabase)
         }
     }
-}
 
+    /**
+     * Apply our font for view
+     */
+    private fun TextView.applyCustomFont() {
+        typeface = Typeface.createFromAsset(assets, "fonts/BadScript-Regular.ttf")
+    }
 
-fun MotionEvent.viewIsTouched(view: View){
-    if(x >= view.left && x <= view.right) {
-        if(y >= view.top && y <= view.bottom) {
-            Log.d("Target", "yeah!")
-            view.setBackgroundColor(Color.GREEN)
-        }
+    /**
+     * Apply margin
+     */
+    private fun LinearLayout.LayoutParams.marginLeftRight() {
+        leftMargin = 30
+        rightMargin = 30
     }
 }
